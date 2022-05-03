2b2371b3e385c860909bbc69ffdd217be9bf624e
Theo Pacun <62911461+tpacun@users.noreply.github.com>
2022-05-02 21:58:11 -0400

    cleaning up main.js

--- js/main.js
+++ js/main.js
@@ -50,28 +50,3 @@ function saveHistory(obj) {
 function saveFavorite(obj) {
   localStorage.setItem(obj.date, 'true')
 }
-
-
-// function getFetch(){
-//   const choice = document.querySelector('input').value
-//   const url = `https://api.nasa.gov/planetary/apod?api_key=8chDM5TyNYtiZItHmQXpukw3SIpAkUaQei3KefuR&date=${choice}`
-
-//   fetch(url)
-//       .then(res => res.json()) // parse response as JSON
-//       .then(data => {
-//         document.querySelector('main').classList.add('fade-in')
-//         document.querySelector('.title').innerText = `Photo of the day for ${data.date}`
-//         document.querySelector('.photoTitle').innerText = data.title
-//         document.querySelector('.author').innerText = data.copyright
-//         document.querySelector('.information').innerText = data.explanation
-//         if(data.media_type === 'image') {
-//           document.querySelector('img').src = data.hdurl
-//         }
-//         else if (data.media_type === 'video') {
-//           document.querySelector('iframe').src = data.url
-//         }
-//       })
-//       .catch(err => {
-//           console.log(`error ${err}`)
-//       });
-// }
